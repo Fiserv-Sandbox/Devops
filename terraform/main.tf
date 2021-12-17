0c380f77369af43ee82ecd9319a9857eebb6d81b
Fija03 <94113056+Fija03@users.noreply.github.com>
2021-12-17 21:31:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeop"
+    storage_account_name = "terraformcodep"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
