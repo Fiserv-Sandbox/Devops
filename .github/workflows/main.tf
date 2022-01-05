42dab2e891eb5003bb6dd85b1af8315b8177ddaa
rkannagala <93792117+rkannagala@users.noreply.github.com>
2022-01-05 11:53:25 -0600

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -5,14 +5,14 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "sahelloazuretfrrk"
+    resource_group_name  = "rg-test-azure-tf"
+    storage_account_name = "satestzuretfrrk"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
+resource "azurerm_resource_group" "rg-test-azure-tf" {
+  name     = "rg-test-azure-tf"
   location = "northcentralus"
 }
