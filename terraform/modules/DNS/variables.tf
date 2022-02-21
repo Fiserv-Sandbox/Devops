ba41e145f62cf4db383490bd2281b1eb5c9f97d9
rkannagala <93792117+rkannagala@users.noreply.github.com>
2022-02-21 14:09:08 -0600

    Add files via upload

---
+++ terraform/modules/DNS/variables.tf
@@ -0,0 +1,18 @@
+variable "rgname" {
+  description = "Resource Group Name"
+  default     = "github-action1"
+  type        = string
+}
+variable "location" {
+  description = "Azure location"
+  default     = "East US"
+  type        = string
+}
+variable "sname" {
+  description = "Azure Storage Account"
+  type        = string
+}
+variable "dns" {
+  description = "Azure DNS Account"
+  type        = string
+}
